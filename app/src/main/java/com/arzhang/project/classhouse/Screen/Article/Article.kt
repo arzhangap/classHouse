ffae59046ace8044a3e9c1b8da5d1155c7f457e5
arjang_ap <arjang.ap@gmail.com>
2023-11-11 16:50:48 +0330

    add progressBar to player

--- app/src/main/java/com/arzhang/project/classhouse/Screen/Article/Article.kt
+++ app/src/main/java/com/arzhang/project/classhouse/Screen/Article/Article.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/arzhang/project/classhouse/ui/components/VideoPlayer.kt
+++ app/src/main/java/com/arzhang/project/classhouse/ui/components/VideoPlayer.kt
@@ -3,22 +3,32 @@ package com.arzhang.project.classhouse.ui.components
 import android.net.Uri
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
@@ -33,19 +43,30 @@ fun Exo(videoUri: String, modifier:Modifier = Modifier) {
                     .build()
                 setMediaItem(mediaItem)
 
-                playWhenReady = true
-                prepare()
+                playWhenReady = false
             }
         }
 
+    var isBuffer by remember { mutableStateOf(false) }
+    exoPlayer.addListener(
+        object : Player.Listener {
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                isBuffer = playbackState == ExoPlayer.STATE_BUFFERING
+            }
+
+            override fun onIsLoadingChanged(isLoading: Boolean) {
+                isBuffer = isLoading
+            }
+        }
+    )
 
         Box(modifier = modifier) {
+            if(isBuffer) {
+                VideoProgressBar(Modifier.align(Alignment.Center))
+            }
             val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
             DisposableEffect(lifecycleOwner) {
                 onDispose { exoPlayer.release() }
-                onDispose {
-                    exoPlayer.release()
-                }
             }
             AndroidView(
                 factory = {
@@ -60,4 +81,12 @@ fun Exo(videoUri: String, modifier:Modifier = Modifier) {
                 }
             )
         }
+}
+
+@Composable
+fun VideoProgressBar(modifier: Modifier = Modifier) {
+    CircularProgressIndicator(
+        modifier = modifier.width(64.dp).zIndex(1F),
+        color = MaterialTheme.colorScheme.surfaceVariant,
+    )
 }
